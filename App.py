f231a873636094b22a36ee0edd4199ddfa4ece9c
Annelies11 <mmahmudi1103@gmail.com>
2024-01-02 23:03:56 +0700

    upload ke tiga

--- App.py
+++ App.py
@@ -14,30 +14,17 @@ warnings.filterwarnings("ignore")
 st.write('# Shop Customer Clustering')
 st.write('Berikut adalah dataset dummy yang berisi karakteristik konsumen sebuah toko.')
 st.write('Pada program ini dataset konsumen ini akan diklaster menggunakan algoritma KMeans.')
-# Specify the data path
 data_path = 'Customers.csv'
-
-# Read the file
-df = pd.read_csv(data_path) # (df is abbreviation for Data Frame)
-
-# Quick look at the data
+df = pd.read_csv(data_path)
 st.write(df)
 
-
-# Quick check
 df.isnull().sum()
 
-# Extracting the count of each profession from the dataframe and storing in profession_dis
 profession_dis = df.Profession.value_counts()
-
-# define the categorical columns.
 categorical_columns = ['Gender', 'Profession']
 
-# Convert or categorical columns to numerical columns.
 for cat_col in categorical_columns:
-  # Initialise label encoder.
   encoder = LabelEncoder()
-  # Apply transformation.
   df[cat_col] = encoder.fit_transform(df[cat_col])
 
 df.drop(columns=['CustomerID'], inplace=True)
@@ -49,12 +36,10 @@ kmeans.fit(X)
 print(kmeans.cluster_centers_)
 df["Cluster"] = kmeans.labels_
 kluster = kmeans.labels_
-# print(kluster)
-# print(df)
-#print(df.iloc[-1])
 st.write('## Data yang telah diklaster')
 st.write('Tiap record akan memiliki label berupa klaster mana dia berada')
 st.write(df)
+st.write('Data yang telah ditraining di atas diubah menjadi bentuk numerik untuk bisa diolah dalam algoritma KMeans')
 st.write('# Masukkan Data yang Akan di Cluster')
 
 gender = st.radio(
@@ -69,11 +54,11 @@ else:
 
 age = st.slider('## Age : ', 0, 100, 25)
 
-income = st.slider('## Annual Income : ', 1000, 200000,50000)
+income = st.slider('## Annual Income ($) : ', 1000, 200000,50000)
 
 s_score = st.slider('## Spending Score : ', 1, 100, 25)
 
-exp = st.slider('## Work Experience : ', 0, 15, 4)
+exp = st.slider('## Work Experience (in year) : ', 0, 15, 4)
 
 fam = st.slider('## Family Size : ', 1, 10, 4)
 
@@ -107,12 +92,14 @@ if bt1 == True :
   st.write("## KMeans Clustering Result")
   X_test.drop(columns=['Cluster'], inplace=True)
   df2.loc[len(df2.index)] = [g, age, income, s_score, p, exp, fam] 
-  # df2.loc[len(df2.index)] = [0, 21, 21000, 21, 2, 1, 2] 
   kmeans_2 = KMeans(n_clusters=3, n_init=10, random_state=42)
   kmeans_2.fit(X_test)
   print(kmeans_2.cluster_centers_)
   df["Cluster"] = kmeans_2.labels_
   st.write(df.iloc[-1])
   bt1 = False
-#st.write(df.iloc[-1])
+
+
+
+
 
